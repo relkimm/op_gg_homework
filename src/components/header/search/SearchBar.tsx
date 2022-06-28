3eff1ce57cc32daffc024324674af92f35905467
김동구 <bbonggu92@gmail.com>
2022-06-29 00:13:21 +0900

    feat: 검색바 검색 버튼 UI 추가

--- src/components/header/search/SearchBar.tsx
+++ src/components/header/search/SearchBar.tsx
@@ -4,6 +4,9 @@ export function SearchBar() {
   return (
     <div className="search-bar">
       <input placeholder="소환사명,챔피언…" />
+      <button>
+        <img src="https://s-lol-web.op.gg/images/icon/icon-gg.svg" alt="" />
+      </button>
     </div>
   );
 }
