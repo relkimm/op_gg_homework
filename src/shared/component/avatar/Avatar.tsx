c6a14d67719f31f576d4f88938c243ad0b9a2d48
김동구 <bbonggu92@gmail.com>
2022-07-02 17:46:36 +0900

    feat: 공통 Avatar 컴포넌트 추가

---
+++ src/shared/component/avatar/Avatar.tsx
@@ -0,0 +1,20 @@
+import { DefaultAvatar } from "../../constant";
+
+type AvatarSize = "sm" | "md" | "lg";
+
+export interface AvatarProps {
+  size: AvatarSize;
+  imageUrl: string;
+}
+
+export function Avatar({ size, imageUrl }: AvatarProps) {
+  const onError = (e: React.SyntheticEvent<HTMLImageElement>) => {
+    e.currentTarget.src = DefaultAvatar();
+  };
+
+  return (
+    <div className={`avatar-${size}`}>
+      <img src={imageUrl} alt="avatar" onError={onError} />
+    </div>
+  );
+}
--- src/shared/constant/index.ts
+++ src/shared/constant/index.ts
@@ -1,3 +1,7 @@
 export const DefaultSummonerIcon = () => {
   return "https://opgg-static.akamaized.net/images/profile_icons/profileIcon2.jpg?image=q_auto&image=q_auto,f_webp,w_auto&v=1656329025328";
 };
+
+export const DefaultAvatar = () => {
+  return "https://opgg-static.akamaized.net/images/lol/champion/Viktor.png?image=c_crop,h_103,w_103,x_9,y_9/q_auto,f_webp,w_auto&v=1656394103685";
+};
